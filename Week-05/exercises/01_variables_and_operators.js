ba59a20768d29d60e188cdcc8fac66ae30e89b1c
melissabonillasellanes <melibonilla3@gmail.com>
2022-09-19 23:28:41 -0300

    BaSP-W05-2022:  Add first exercises related to variables and operators.

--- Week-05/exercises/01_variables_and_operators.js
+++ Week-05/exercises/01_variables_and_operators.js
@@ -5,12 +5,30 @@ console.log("--EXERCISE 1: VARIABLES AND OPERATORS");
 
 console.log("-Exercise 1.a:");
 
+var number1 = 20
+var number2 = 6
+var result = number1 + number2
+
+console.log(result)
+
 // Crear dos variables de tipo String y concatenarlas guardando
 // el resultado en una 3er variable.
 
 console.log("-Exercise 1.b:")
 
+var string1 = "Hola"
+var string2 = "Mundo"
+var resuls = string1 + " " + string2
+
+console.log(resuls)
+
 // Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras
 // del string) guardando el resultado de la suma en una 3er variable (utilizar length).
 
 console.log("-Exercise 1.c:")
+
+var typeString1 = "Buenos días,"
+var typeString2 = "Que lindo día"
+var results1c = typeString1.length + typeString2.length
+
+console.log(results1c)
