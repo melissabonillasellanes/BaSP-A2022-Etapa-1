4cd95eb5376e0d5ada7175fae54838015e3dd09a
melissabonillasellanes <melibonilla3@gmail.com>
2022-10-04 20:24:08 -0300

    BaSP-W07: Fetch was entered for the sign up

--- Week-07/javascript/sign-up.js
+++ Week-07/javascript/sign-up.js
@@ -14,6 +14,20 @@ window.onload = function () {
     var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
     var array = [firstName, lastName, dni, birthDate, phoneNumber, address, location, postalCode, email, password, repeatPassword];
 
+    if (localStorage.length > 0) {
+        firstName.value = localStorage.getItem("name");
+        lastName.value = localStorage.getItem("lastName");
+        dni.value = localStorage.getItem("dni");
+        birthDate.value = dateFormat(localStorage.getItem("dob"), false);
+        phoneNumber.value = localStorage.getItem("phone");
+        address.value = localStorage.getItem("address");
+        location.value = localStorage.getItem("city");
+        postalCode.value = localStorage.getItem("zip");
+        email.value = localStorage.getItem("email");
+        password.value = localStorage.getItem("password");
+        repeatPassword.value=localStorage.getItem("password");
+    };
+
     array.forEach(function(element) {
         element.addEventListener("blur", function() {
             validation(element);
@@ -92,7 +106,7 @@ window.onload = function () {
 
     function checkFirstName () {
         if (checkOut(firstName)) {
-            return "Incomplete field name";
+            return "Incomplete field name \n";
         } else if (firstName.value.length < 4) {
             showError(firstName, "This field must contain at least 4 characters");
             return "invalid character numbers";
@@ -106,7 +120,7 @@ window.onload = function () {
 
     function checkLastName () {
         if (checkOut(lastName)) {
-            return "Incomplete surname field";
+            return "Incomplete surname field \n";
         } else if (lastName.value.length < 4) {
             showError(lastName, "This field must contain at least 4 characters");
             return "invalid character numbers";
@@ -120,7 +134,7 @@ window.onload = function () {
 
     function checkDni() {
         if (checkOut(dni)) {
-            return "Incomplete ID field";
+            return "Incomplete ID field \n";
         } else if (dni.value.length < 8) {
             showError(dni, "This field must contain more than 7 numbers");
             return "Wrong, ID with less than 7 numbers";
@@ -134,7 +148,7 @@ window.onload = function () {
 
     function checkBirthDate() {
         if(checkOut(birthDate)) {
-            return "Incomplete date of birth field";
+            return "Incomplete date of birth field \n";
         } else if ((new Date(birthDate.value)) >= new Date()) {
             showError(birthDate, "The date entered is after the current date");
             return "Birth date is after the current date";
@@ -143,9 +157,31 @@ window.onload = function () {
         };
     };
 
+    function dateFormat (value, format) {
+        var date = new Date(value);
+        var dd = date.getDate() + 1;
+        var mm = date.getMonth() + 1;
+        var aaaa = date.getFullYear();
+        if (dd < 10) {
+            dd = "0" + dd;
+        };
+        if (mm < 10) {
+            mm = "0" + mm;
+        };
+        if (format) {
+            return mm + "/" + dd + "/" + aaaa;
+        } else {
+            dd = Number(dd) - 1;
+            if (dd < 10) {
+                dd = "0" + dd;
+            };
+            return aaaa + "-" + mm + "-" + dd;
+        };
+    };
+
     function checkPhoneNumber() {
         if(checkOut(phoneNumber)) {
-            return "Incomplete phone number field";
+            return "Incomplete phone number field \n";
         } else if (isNaN(phoneNumber.value) || characterValidate(phoneNumber.value)) {
             showError(phoneNumber, "Please insert only numbers");
             return "Invalid phone number";
@@ -159,7 +195,7 @@ window.onload = function () {
 
     function checkAddress () {
         if(checkOut(address)) {
-            return "Incomplete address field";
+            return "Incomplete address field \n";
         } else if (address.value.length <= 5 || address.value.trim().split(" ").length < 2) {
             showError(address, "This field must contain at least 5 characters with a space in between");
             return "Invalid address name";
@@ -173,7 +209,7 @@ window.onload = function () {
 
     function checkLocation() {
         if(checkOut(location)) {
-            return "Incomplete location field";
+            return "Incomplete location field \n";
         } else if (location.value.length <= 4) {
             showError(location, "This field must contain at least 4 characters");
             return "Location name with less than 4 characters";
@@ -187,7 +223,7 @@ window.onload = function () {
 
     function checkPostalCode() {
         if(checkOut(postalCode)) {
-            return "Incomplete postal code field";
+            return "Incomplete postal code field \n";
         } else if (isNaN(postalCode.value) || characterValidate(postalCode.value)) {
             showError(postalCode, "Please enter a number");
             return "Invalid postal code";
@@ -201,7 +237,7 @@ window.onload = function () {
 
     function checkEmail () {
         if (checkOut(email)) {
-            return "Incomplete email field";
+            return "Incomplete email field \n";
         } else if (!emailExpression.test(email.value)) {
             showError(email, "Please insert a valid email");
             return "Invalid email format";
@@ -212,7 +248,7 @@ window.onload = function () {
 
     function checkPassword () {
         if (checkOut(password)) {
-            return "Incomplete password field";
+            return "Incomplete password field \n";
         } else if (password.value.length < 8) {
             showError(password, "This field must contain at least 8 characters");
             return "Password with less than 8 characters";
@@ -247,6 +283,24 @@ window.onload = function () {
         container.className = "validation";
     };
 
+    function saveLocal(res) {
+    Object.entries(res.data).forEach(element => {
+        localStorage.setItem(element[0], element[1]);
+    });
+};
+
+function displayError(res) {
+    var alertError = [];
+    if (res.hasOwnProperty("errors")) {
+        Object.entries(res.errors).forEach(element => {
+            alertError += "\n" + element[1].msg;
+        });
+        alert("Sorry an error occurred. Please check the following fields: " + alertError);
+    } else {
+        alert("Sorry an error has occurred: " + res.msg);
+    };
+};
+
     button.addEventListener("click", function(btn) {
         btn.preventDefault();
         array.forEach(function(element) {
@@ -256,9 +310,9 @@ window.onload = function () {
         var validations = [checkFirstName(), checkLastName (), checkDni(), checkBirthDate(), checkPhoneNumber(), checkPostalCode()
         , checkAddress(), checkLocation(), checkPassword(), checkEmail (), checkRepeatPassword ()];
         var errors = [];
-        for (var i=0; i<validations.length; i++) {
+        for (var i=0; i < validations.length; i++) {
             if (validations[i] !== "") {
-                errors += "\n" + validations[i];
+                errors += validations[i];
             };
         };
         if (errors == "") {
@@ -266,8 +320,27 @@ window.onload = function () {
             "\n DNI: " + dni.value + "\n Birth Date: " + birthDate.value + "\n Phone Number: "
             + phoneNumber.value + "\n Address: " + address.value + "\n Location: " + location.value + "\n Postal Code: "
             + postalCode.value + "\n Email: " + email.value + "\n Password: " + password.value);
-        } else {
-            alert("The user could not be created. Please check:" + errors);
-        };
-    });
+
+        fetch("https://basp-m2022-api-rest-server.herokuapp.com/signup?name=" + firstName.value  + "&lastName=" +
+        lastName.value + "&dni=" + dni.value + "&dob=" + dateFormat(birthDate.value, true) + "&phone=" +
+        phoneNumber.value + "&address=" + address.value + "&city=" + location.value + "&zip=" + postalCode.value
+        + "&email=" + email.value + "&password=" + password.value)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (jsonResponse) {
+            if (jsonResponse.success) {
+                alert("The request was successful: " + jsonResponse.msg);
+                saveLocal(jsonResponse);
+            } else {
+            throw jsonResponse
+            }
+        })
+        .catch(function (error) {
+            displayError(error);
+        });
+    } else {
+        alert("Sorry, the user could not be created. Please check the following fields: \n" + errors);
+    };
+});
 };
